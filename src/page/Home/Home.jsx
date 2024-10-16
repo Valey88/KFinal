b8651aa37036904fc13605491509c500d77eeef2
SU <valej200554@gmail.com>
2024-10-16 13:10:01 +0500

    fix header and maps

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -19,19 +19,11 @@ import "../../components/slider/styles.module.css";
 
 // Import images
 import frame1 from "/public/Frame 1.svg";
-import img1 from "/public/91a921c4084d1fd7cf2fd71448204ba0.png";
-import img2 from "/public/52b21197-05f6-4054-b9f4-95d5d760b078.jpg";
-import maskGroup1 from "/public/adc6b44f-97e2-4997-8d10-cb11db5e4fa6.jpg";
-import maskGroup2 from "/public/a24a3dd0-a79f-4f25-b36c-3f47491987ef.jpg";
-import eventImg from "/public/event.png";
-import wifiImg from "/public/wifi (1).png";
-import internetImg from "/public/internet.png";
-import armchairImg from "/public/armchair.png";
-import locationImg from "/public/Group 119 (1).png";
 import roomImg from "/public/4eb86257-5851-4f13-9ae0-b24fd64cae6b.jpg";
 import { Carousel } from "react-bootstrap"; // Импорт Carousel из react-bootstrap
 
-// Import Bootstrap CSS and JS
+import homeInfo from "/public/home-info.json";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { YMaps, Map, Placemark } from "react-yandex-maps";
@@ -46,186 +38,116 @@ const Home = () => {
           <div>
             <img src={frame1} alt="" loading="lazy" />
           </div>
-          <h2>Выбирай лучшее места коворкинга в городе</h2>
+          <h2>{homeInfo.about_us.title}</h2>
           <Link to="/Booking" className={style.Link}>
-            Забронировать
+            {homeInfo.about_us.button_text}
           </Link>
         </div>
         <div className={style.contentOne2}>
-          <img src={img1} alt="" loading="lazy" />
+          <img src={homeInfo.about_us.image} alt="" loading="lazy" />
         </div>
       </div>
       <div className={style.component}>
         <div className={style.content_component2}>
-          <img src={img2} alt="" loading="lazy" />
+          <img
+            src={homeInfo.about_us_description.image}
+            alt=""
+            loading="lazy"
+          />
         </div>
         <div className={style.content_component}>
-          <h2>Самые удобные пространства</h2>
-          <p>
-            Коворкинг — это соглашение, при котором сотрудники разных компаний
-            делят одно офисное помещение, что обеспечивает экономию средств и
-            удобство за счет использования общей инфраструктуры, такой как
-            оборудование, коммунальные услуги, услуги администратора и хранения,
-            а в некоторых случаях услуги по прохладительным напиткам и приему
-            посылок.
-          </p>
+          <h2>{homeInfo.about_us_description.title}</h2>
+          <p>{homeInfo.about_us_description.description}</p>
         </div>
       </div>
       <div className={style.component2}>
-        <div className={style.content_component3}>
-          <div>
-            <p>Описание</p>
-          </div>
-          <h2>Представление всего опыт технологии</h2>
-          <p>
-            Коворкинг — это соглашение, при котором сотрудники разных компаний
-            делят одно офисное помещение, что обеспечивает экономию средств и
-            удобство за счет использования общей инфраструктуры, такой как
-            оборудование, коммунальные услуги, услуги администратора и хранения,
-            а в некоторых случаях услуги по прохладительным напиткам и приему
-            посылок.
-          </p>
-        </div>
-        <div className={style.content_component4}>
-          <Carousel>
-            {/* ... existing Carousel code ... */}
-            <Carousel.Item
-              style={{
-                width: "100%",
-                height: "60rem",
-                maxHeight: "60vh", // Добавлено для мобильных устройств
-              }}
-              key="maskGroup1"
-            >
-              <img
-                className="d-block"
-                src={maskGroup1} // Use the imported image
-                alt="Mask Group 1"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            </Carousel.Item>
-            <Carousel.Item
-              style={{
-                width: "100%",
-                height: "60rem",
-                maxHeight: "60vh", // Добавлено для мобильных устройств
-              }}
-              key="maskGroup2"
-            >
-              <img
-                className="d-block"
-                src={maskGroup2} // Use the imported image
-                alt="Mask Group 2"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-              <img
-                className="d-block w-100"
-                src="default-image.jpg"
-                alt="No image available"
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            </Carousel.Item>
-          </Carousel>
-        </div>
+        {homeInfo.description.items.map((date) => (
+          <>
+            <div className={style.content_component3}>
+              <div>
+                <p>Описание</p>
+              </div>
+              <h2>{date.title}</h2>
+              <p>{date.description}</p>
+            </div>
+            <div className={style.content_component4}>
+              <Carousel>
+                {homeInfo.description.items[0].slider.images.map(
+                  (image, index) => (
+                    <Carousel.Item
+                      style={{
+                        width: "100%",
+                        height: "60rem",
+                        maxHeight: "60vh", // Добавлено для мобильных устройств
+                      }}
+                      key="maskGroup1"
+                    >
+                      <img
+                        className="d-block"
+                        src={image} // Use the imported image
+                        alt="Mask Group 1"
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </Carousel.Item>
+                  )
+                )}
+              </Carousel>
+            </div>
+          </>
+        ))}
       </div>
       <div className={style.component3}>
         <div className={style.content_component5} id="prem">
           <p>Преимущества</p>
-          <h2>Выбирай наш комфорт</h2>
+          <h2>{homeInfo.advantages.title}</h2>
           <div className={style.content_components}>
-            <div className={style.contents}>
-              <div>
-                <img src={eventImg} alt="" loading="lazy" />
+            {homeInfo.advantages.items.map((data) => (
+              <div className={style.contents}>
+                <div>
+                  <img src={data.image} alt="" loading="lazy" />
+                </div>
+                <h3> {data.title}</h3>
+                <p>{data.description}</p>
               </div>
-              <h3> Мероприятия</h3>
-              <p>
-                Обеспечение экономии средств и удобства за счет использования
-                общей инфраструктуры.
-              </p>
-            </div>
-            <div className={style.contents}>
-              <div>
-                <img src={wifiImg} alt="" loading="lazy" />
-              </div>
-              <h3>Быстрый интернет</h3>
-              <p>
-                Обеспечение экономии средств и удобства за счет использования
-                общей инфраструктуры.
-              </p>
-            </div>
-            <div className={style.contents}>
-              <div>
-                <img src={internetImg} alt="" loading="lazy" />
-              </div>
-              <h3>Доступность</h3>
-              <p>
-                Обеспечение экономии средств и удобства за счет использования
-                общей инфраструктуры.
-              </p>
-            </div>
-            <div className={style.contents}>
-              <div>
-                <img src={armchairImg} alt="" loading="lazy" />
-              </div>
-              <h3>Лаундж</h3>
-              <p>
-                Обеспечение экономии средств и удобства за счет использования
-                общей инфраструктуры.
-              </p>
-            </div>
+            ))}
           </div>
         </div>
       </div>
       <div className={style.component4}>
         <h2>Помещения</h2>
         <div className={style.content_component6}>
-          <img src={roomImg} alt="" loading="lazy" />
+          <img src={homeInfo.rooms.image} alt="" loading="lazy" />
         </div>
         <div className={style.content_component7}>
           <div>
             <p>Описание</p>
           </div>
-          <h2>Представление всего опыт технологии</h2>
-          <p>
-            Коворкинг — это соглашение, при котором сотрудники разных компаний
-            делят одно офисное помещение, что обеспечивает экономию средств и
-            удобство за счет использования общей инфраструктуры, такой как
-            оборудование, коммунальные услуги, услуги администратора и хранения,
-            а в некоторых случаях услуги по прохладительным напиткам и приему
-            посылок.
-          </p>
+          <h2>{homeInfo.rooms.title}</h2>
+          <p>{homeInfo.rooms.description}</p>
           <Link to="/Booking" className={style.Link}>
-            Подробнее
+            {homeInfo.rooms.button_text}
           </Link>
         </div>
       </div>
       <div className={style.component5}>
         <div className={style.content_component8}>
           <div>
-            <h3>Местоположение</h3>
-            <p>Оренбург, Советская улица, 6</p>
+            <h3>{homeInfo.map.title}</h3>
+            <p>{homeInfo.map.address}</p>
           </div>
           <div className={style.mapContainer}>
             <YMaps>
               <Map
                 defaultState={{ center: [51.756253, 55.10584], zoom: 15 }}
                 width="100%"
-                height="400px"
-                options={{ 
-                  autoFitToViewport: true, // Добавлено для адаптивности
-                  suppressMapOpenBlock: true // Скрыть блок открытия карты
+                height="100vh" // или height="50vh" для более компактной карты
+                options={{
+                  autoFitToViewport: true,
+                  suppressMapOpenBlock: true,
                 }}
               >
                 <Placemark geometry={[51.756253, 55.10584]} />
@@ -234,21 +156,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      {/* {data?.map((data) => {
-        return (
-          <div key={data.id}>
-            <h2>{data.address}</h2>
-            <h2>Забронировать</h2>
-            <input type="text" placeholder="" />
-            <input type="text" placeholder=""/>
-            <input type="text" placeholder=""/>
-            <input type="text" placeholder=""/>
-            <input type="text" placeholder=""/>
-            <input type="text" placeholder=""/>
-          </div>
-        );
-      })} */}
     </div>
   );
 };
